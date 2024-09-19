ba03200481e1d0175b6e1241155646a89a6341fb
deepikakushwah12 <kushwahdeepika56@gmail.com>
2024-09-19 19:05:53 +0530

    Add files via upload

---
+++ petten/Q13.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+{
+
+   int i,j,k='A';
+    for(i='A';i<='E';i++) 
+    {
+    	for(j='A';j<=i;j++)
+    	{
+    		printf("%c ",k);
+    		k++;
+		}
+		printf("\n");
+	}
+	
+}
